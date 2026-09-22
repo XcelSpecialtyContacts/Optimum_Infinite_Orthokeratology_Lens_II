53d7552bccac13420772a9e35972f94b13b49c8e
Douglas Gilliard <agilliard2@walman.com>
2026-09-22 10:42:13 -0400

    Added files for investigation of power targetting issue

--- call_oiol2_point_file_gen.bat
+++ call_oiol2_point_file_gen.bat
@@ -6,14 +6,32 @@ REM ============================================
 set WO=%2
 
 REM ============================================
-REM Move to required working directory
+REM Set paths
 REM ============================================
-cd /d D:\oiol2
+set "OIOL2_HOME=D:\xpf\oiol2"
+set "PY32=D:\Python32\python.exe"
+
+if not exist "%PY32%" (
+    echo ERROR: 32-bit Python not found at %PY32% 1>&2
+    exit /b 1
+)
+
+if not exist "%OIOL2_HOME%\src\oiol2\main.py" (
+    echo ERROR: oiol2 package not found at %OIOL2_HOME%\src\oiol2\main.py 1>&2
+    exit /b 1
+)
+
+REM ============================================
+REM Move to required working directory
+REM Set the working directory
+REM ============================================
+cd /d "%OIOL2_HOME%"
+set "PYTHONPATH=%OIOL2_HOME%\src"
 
 REM ============================================
 REM Execute Python point file generator
 REM ============================================
-D:\Python32\python.exe -m oiol2.main --wo %WO%
+"%PY32%" -m oiol2.main --wo %WO%
 
 REM ============================================
 REM Capture Python exit code
@@ -23,4 +41,4 @@ set EXITCODE=%ERRORLEVEL%
 REM ============================================
 REM Return same exit code to calling process
 REM ============================================
-exit /b %EXITCODE%
+endlocal & exit /b %EXITCODE%
